450ed8b2d0c04db08f2113e2c6cd43ff503276bd
Ashlyn Lanaville <ashlyn@downing.us>
2022-04-21 14:43:38 -0700

    added trips to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: {type: String, unique: true, required: true, minlength: 2},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    trips: [{type: Schema.Types.ObjectId, ref: 'User', required: false}]
 }, {timestamps: true})
 
 const User = mongoose.model('User', userSchema);
