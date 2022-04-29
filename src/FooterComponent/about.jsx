f9aabd9208365016681e897d5336448df12793c6
Ashlyn Lanaville <ashlyn@downing.us>
2022-04-29 13:51:49 -0700

    added a little p tag in about page

--- src/FooterComponent/about.jsx
+++ src/FooterComponent/about.jsx
@@ -1,7 +1,8 @@
 const About = () => {
     return (
         <div>
-            <h1>About yay</h1>
+            <h1>About</h1>
+            <p>Trip Planner is an app to help you plan all the details of your trip all on one site! Add people you know to the trip and they can also edit it! </p>
         </div>
     )
 }
